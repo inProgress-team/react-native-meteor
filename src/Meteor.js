2b4a061b1dccded91b218e1665ba818e9a0b9028
Farid from Safi <farid.lazreq@gmail.com>
2016-06-17 11:42:16 +0200

    keep context in case of future needs

--- src/Meteor.js
+++ src/Meteor.js
@@ -109,7 +109,9 @@ module.exports = {
       Data.notify('change');
 
       console.info("Connected to DDP server.");
-      this._loadInitialUser().then(this._subscriptionsRestart);
+      this._loadInitialUser().then(() => {
+        this._subscriptionsRestart();
+      });
     });
 
     let lastDisconnect = null;
