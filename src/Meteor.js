e6c566ffdf7494be64d1f974b7327c1da426a805
baryshok <baryshok@gmail.com>
2016-12-12 19:22:25 +0300

    Enable processing of 'cleared' param while subscribing/unsubscribing

--- src/Meteor.js
+++ src/Meteor.js
@@ -25,9 +25,6 @@ import ReactiveDict from './ReactiveDict';
 import User from './user/User';
 import Accounts from './user/Accounts';
 
-const subscribing = [];
-const unsubscribing = [];
-
 
 module.exports = {
   composeWithTracker,
@@ -68,9 +65,7 @@ module.exports = {
     for(var i in Data.subscriptions) {
       const sub = Data.subscriptions[i];
       Data.ddp.unsub(sub.subIdRemember);
-      unsubscribing.push(sub.subIdRemember);
       sub.subIdRemember = Data.ddp.sub(sub.name, sub.params);
-      subscribing.push(sub.subIdRemember);
     }
 
   },
@@ -144,14 +139,6 @@ module.exports = {
     });
 
     Data.ddp.on("ready", message => {
-      let subs = message.subs;
-      subs && subs.forEach(subId => {
-        let subIndex = subscribing.findIndex(item => item === subId);
-        if (subIndex !== -1) {
-          subscribing.splice(subIndex, 1);
-        }
-      });
-
       const idsMap = new Map();
       for(var i in Data.subscriptions) {
         const sub = Data.subscriptions[i];
@@ -170,17 +157,12 @@ module.exports = {
 
     Data.ddp.on("changed", message => {
       let fields = {...message.fields};
-      let cleared;
-
-      if (!subscribing.length && !unsubscribing.length) {
-        cleared = message.cleared;
-        if (cleared && cleared.length) {
-          cleared.forEach(fieldName => {
-            fields[fieldName] = null;
-          });
-        }
+      let cleared = message.cleared;
+      if (cleared && cleared.length) {
+        cleared.forEach(fieldName => {
+          fields[fieldName] = null;
+        });
       }
-
       Data.db[message.collection] && Data.db[message.collection].upsert({_id: message.id, ...fields});
     });
 
@@ -194,12 +176,6 @@ module.exports = {
     });
 
     Data.ddp.on("nosub", message => {
-      let subId = message.id;
-      let subIndex = unsubscribing.findIndex(item => item === subId);
-      if (subIndex !== -1) {
-        unsubscribing.splice(subIndex, 1);
-      }
-
       for(var i in Data.subscriptions) {
         const sub = Data.subscriptions[i];
         if(sub.subIdRemember == message.id) {
@@ -272,7 +248,6 @@ module.exports = {
 
       id = Random.id();
       const subIdRemember = Data.ddp.sub(name, params);
-      subscribing.push(subIdRemember);
 
       Data.subscriptions[id] = {
         id: id,
@@ -286,7 +261,6 @@ module.exports = {
         stopCallback: callbacks.onStop,
         stop: function() {
           Data.ddp.unsub(this.subIdRemember);
-          unsubscribing.push(this.subIdRemember);
           delete Data.subscriptions[this.id];
           this.ready && this.readyDeps.changed();
 
