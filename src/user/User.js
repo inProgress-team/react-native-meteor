aef55f4bb95f192c3e93d3ab77ab71eecaab438a
Hong Phong <spkt06@gmail.com>
2019-06-10 17:27:06 +0700

    Change import AsyncStorage
    Use AsyncStorage from @react-native-community/async-storage instead from react-native

--- src/user/User.js
+++ src/user/User.js
@@ -1,4 +1,4 @@
-import { AsyncStorage } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 
 import Data from '../Data';
 import { hashPassword } from '../../lib/utils';
